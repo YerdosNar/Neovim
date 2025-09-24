a9225d8baad4f390ab2fc40777bf204b626629d6
YerdosNar <besway2000@gmail.com>
2025-09-24 14:44:44 +0900

    alpha updated with new image

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,27 +1,28 @@
 return {
-
     "goolord/alpha-nvim",
-
-    -- dependencies = { 'echasnovski/mini.icons' },
-
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
     config = function()
-
-        local startify = require("alpha.themes.startify")
-
-        -- available: devicons, mini, default is mini
-
-        -- if provider not loaded and enabled is true, it will try to use another provider
-
-        startify.file_icons.provider = "devicons"
-
-        require("alpha").setup(
-
-            startify.config
-
-        )
-
+        local alpha = require("alpha")
+        local dashboard = require("alpha.themes.startify")
+        dashboard.section.header.val = {
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                     ]],
+            [[       ████ ██████           █████      ██                     ]],
+            [[      ███████████             █████                             ]],
+            [[      █████████ ███████████████████ ███   ███████████   ]],
+            [[     █████████  ███    █████████████ █████ ██████████████   ]],
+            [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+            [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+            [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+        }
+        alpha.setup(dashboard.opts)
     end,
-
 }
