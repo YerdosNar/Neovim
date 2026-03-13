f30c857a7d5c194f8067ed6f6c178b86f3e8265b
YerdosNar <besway2000@gmail.com>
2026-03-13 14:43:38 +0900

    Updated markdown.lua file, now it uses 'selimacerbas/markdown-preview.nvim'

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,9 +1,14 @@
 -- /home/karina/.config/nvim/lua/plugins/markdown.lua
 return {
-    {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        ft = { "markdown" },
-        build = function() vim.fn["mkdp#util#install"]() end,
-    }
+    "selimacerbas/markdown-preview.nvim",
+    dependencies = { "selimacerbas/live-server.nvim" },
+    config = function()
+        require("markdown_preview").setup({
+            -- all optional; sane defaults shown
+            instance_mode = "takeover",  -- "takeover" (one tab) or "multi" (tab per instance)
+            port = 0,                    -- 0 = auto (8421 for takeover, OS-assigned for multi)
+            open_browser = true,
+            debounce_ms = 300,
+        })
+    end,
 }
