3559bff819dcc728dd12d612ab664cd7d0e7042b
YerdosNar <besway2000@gmail.com>
2026-04-05 02:00:52 +0900

    Fallback to 'iamcco/markdown-preview.nvim' for MarkDownPreview

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,8 +12,8 @@ vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 -- Indentation
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 8
+vim.opt.shiftwidth = 8
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 
--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,14 +1,10 @@
 -- /home/karina/.config/nvim/lua/plugins/markdown.lua
 return {
-    "selimacerbas/markdown-preview.nvim",
-    dependencies = { "selimacerbas/live-server.nvim" },
-    config = function()
-        require("markdown_preview").setup({
-            -- all optional; sane defaults shown
-            instance_mode = "takeover",  -- "takeover" (one tab) or "multi" (tab per instance)
-            port = 0,                    -- 0 = auto (8421 for takeover, OS-assigned for multi)
-            open_browser = true,
-            debounce_ms = 300,
-        })
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+        vim.g.mkdp_filetypes = { "markdown" }
     end,
+    ft = { "markdown" },
 }
